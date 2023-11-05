45a120adb0c1b2ddad881df78822877aab97bafe
Masud_Rahman22 <masudrhmn6560@gmail.com>
2023-11-05 15:46:09 +0600

    another section called exlpore blogging is created with swiper js and the logo is resized

--- src/MainLayout/Banner/Banner.jsx
+++ src/MainLayout/Banner/Banner.jsx
@@ -1,8 +1,5 @@
-
-
 import animation from '../../../public/animation_lol7hhg6.json'
 import Lottie from 'lottie-react';
-
 
 const Banner = () => {
     return (
@@ -10,7 +7,6 @@ const Banner = () => {
             <div className="min-h-screen">
                 <div className="flex flex-col lg:flex-row-reverse items-center justify-between">
                     <Lottie className='flex-1' animationData={animation}></Lottie>
-                    {/* <img src={picture} className="max-w-sm rounded-lg" /> */}
                     <div className='flex-1 mx-auto text-left lg:ml-10' data-aos="fade-right">
                         <h1 className='text-4xl font-semibold mt-10 font-merienda'><span className='text-blue-500 font-merienda'>Discover</span> Endless Inspiration</h1>
                         <p className='text-xl font-extralight mt-5'>Unleash your curiosity and immerse yourself in a world of captivating stories, insightful articles, and thought-provoking ideas. Our blog is your gateway to inspiration, knowledge, and endless possibilities. Whether you are a seasoned reader or just starting your journey, we have got something for everyone.</p>
